1de689324c84cdb9ab42bd8447229d54e3df7be9
camilo1210 <camilopavasrios@gmail.com>
2024-05-24 16:46:39 -0500

    Cambios en Restaurante

--- Concurrencias/src/concurrencias/Restaurante.java
+++ Concurrencias/src/concurrencias/Restaurante.java
@@ -12,32 +12,23 @@ public class Restaurante {
         Cocinero[] cocineros = new Cocinero[3];
         
         
-        int Clientes = Cantidad
+        int NumeroClientes = Integer.parseInt(JOptionPane.showInputDialog("ingrese el numero de Clientes"));
         
         for (int i = 0; i < cocineros.length; i++) {
             cocineros[i] = new Cocinero(estufa);
             Thread t = new Thread(cocineros[i]);
             t.start();
         }
+        
+        for (int i = 1; i < NumeroClientes; i++) {
+            Cliente cliente = new Cliente("Cliente "+i, cajero, armador, cocineros);
+            Thread tN = new Thread(cliente);
 
-        Cliente cliente1 = new Cliente("Cliente 1", cajero, armador, cocineros);
-        Cliente cliente2 = new Cliente("Cliente 2", cajero, armador, cocineros);
-        Cliente cliente3 = new Cliente("Cliente 3", cajero, armador, cocineros);
+            tN.start();
+        }
 
-        Thread t1 = new Thread(cliente1);
-        Thread t2 = new Thread(cliente2);
-        Thread t3 = new Thread(cliente3);
-
-        t1.start();
-        t2.start();
-        t3.start();
     }
     
     
-    public int CantidadClientes (){
-        int CantidadClientes = Integer.parseInt(JOptionPane.showInputDialog("ingrese el numero de Clientes"));
-        
-        return CantidadClientes;
-        
-    }
+   
 }
