1b2384e9cb4ec2899c432d002d738594e2b582ce
Andrick Buitrago <sterraquio@hotmail.com>
2024-05-24 18:11:14 -0500

    Listo

--- Concurrencias/src/concurrencias/Cliente.java
+++ Concurrencias/src/concurrencias/Cliente.java
@@ -3,25 +3,73 @@ package concurrencias;
 import java.util.concurrent.Semaphore;
 
 public class Cliente implements Runnable {
+
     private String nombre;
     private Cajero cajero;
     private Armador armador;
     private Cocinero[] cocineros;
+    private String pedido = "";
+
+    String dobleCarne;
+    String Picante;
+    String lechuga;
+    String Tomate;
+    String Cebolla;
+    String Salsas;
+    int quiereOno;
 
     public Cliente(String nombre, Cajero cajero, Armador armador, Cocinero[] cocineros) {
         this.nombre = nombre;
         this.cajero = cajero;
         this.armador = armador;
         this.cocineros = cocineros;
+
     }
 
     @Override
     public void run() {
         try {
+            //Casos para saber si quiere doble carne, picante, lechuga 
+            if (quiereONoquiere() == 1) {
+                dobleCarne = "quiere doble carne";
+            } else {
+                dobleCarne = "no Quiere doble carne";
+            }
+
+            if (quiereONoquiere() == 1) {
+                Picante = "quiere picante";
+            } else {
+                Picante = "No quiere picante";
+            }
+
+            if (quiereONoquiere() == 1) {
+                lechuga = "quiere lechuga";
+            } else {
+                lechuga = "no quiere lechuga";
+            }
+            if (quiereONoquiere() == 1) {
+                Tomate = "quiere tomate";
+            } else {
+                Tomate = "no quiere tomate";
+            }
+
+            if (quiereONoquiere() == 1) {
+                Cebolla = "quiere cebolla";
+            } else {
+                Cebolla = "no quiere cebolla";
+            }
+
+            if (quiereONoquiere() == 1) {
+                Salsas = "quiere salsas";
+            } else {
+                Salsas = "no quiere salsas";
+            }
+
             // El cliente llega
             System.out.println(nombre + " llega al restaurante.");
             // Hace un pedido
-            String pedido = "Pedido de " + nombre;
+            pedido = "Pedido de " + nombre + " " + dobleCarne + " " + Picante + " " + lechuga
+                    + " " + Tomate + " " + Cebolla + " " + Salsas;
             System.out.println(nombre + " hace un pedido: " + pedido);
             cajero.recibirPedido(pedido);
             // Espera a que le entreguen su pedido
@@ -36,5 +84,12 @@ public class Cliente implements Runnable {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public int quiereONoquiere() {
+        double temporal = Math.random();
+        quiereOno = (int) Math.round(temporal);
+
+        return quiereOno;
     }
 }
